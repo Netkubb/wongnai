eec32e864fcbbe1c03fe8f43718908378ca993d2
Netkubb <57309336+Netkubb@users.noreply.github.com>
2022-10-05 05:52:02 +0700

    layout grid

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import {Box,Grid} from '@mui/material'
 import Header from '../comps/Header'
 import Navbar from '../comps/Navbar'
 import styles from '../styles/Home.module.css'
@@ -8,5 +9,31 @@ export default function Home() {
   return (<>
     <Header/>
     <Navbar/>
+    <Box style={{backgroundColor:"#EDF0F2"}}>
+      <Grid container columnSpacing={2} style={{width:"80vw",display:"flex",justifyContent:"center",margin:"auto"}}>
+        <Grid item xs={2} style={{backgroundColor:"white",borderRadius:"5px"}}>
+          <div>1</div>
+        </Grid>
+        <Grid item xs={10}>
+          <Grid container rowGap={2} direction={"row"}>
+            <Grid item xs={12} style={{backgroundColor:"black",borderRadius:"5px",height:"134px"}}>
+              <div>2</div>
+            </Grid>
+            <Grid item xs={12}>
+              <Grid container>
+                <Grid item xs={7} style={{backgroundColor:"white",borderRadius:"5px"}}>
+                  <div>3</div>
+                </Grid>
+                <Grid item xs={1}>
+                </Grid>
+                <Grid item xs={4} style={{backgroundColor:"red",borderRadius:"5px"}}>
+                  <div>4</div>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Box>
   </>)
 }
