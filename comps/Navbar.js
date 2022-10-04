6794152bf9d379e2d1535336233fab9fca6d07e5
Netkubb <57309336+Netkubb@users.noreply.github.com>
2022-10-05 05:08:59 +0700

    navbar

---
+++ comps/Navbar.js
@@ -0,0 +1,28 @@
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import {Typography,Link,Box,AppBar} from '@mui/material'
+
+
+export default function Navbar(){
+    return (
+        <Box>
+            <AppBar color={"transparent"} position="static" style={{padding:"3vh 10vw",minHeight:"48px",boxShadow: "none"}}  >
+                <Breadcrumbs aria-label="breadcrumb">
+                    <Link underline="hover" color="inherit" href="/">
+                        หน้าแรก
+                    </Link>
+                    <Link
+                        underline="hover"
+                        color="inherit"
+                        href="/material-ui/getting-started/installation/"
+                    >
+                        ร้านอาหาร
+                    </Link>
+                    <Typography color="text.primary">ค้นหาร้านอาหาร</Typography>
+                </Breadcrumbs>
+                <Typography variant='h5' style={{paddingTop:"1vh"}}>
+                    <b>ร้านอาหาร ยอดนิยม</b>
+                </Typography>
+            </AppBar>
+        </Box>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../comps/Header'
+import Navbar from '../comps/Navbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (<>
     <Header/>
+    <Navbar/>
   </>)
 }
